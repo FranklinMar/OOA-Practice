d4c24e077dd878e1362171615f86e0cc32989835
FranklinMar <49430756+FranklinMar@users.noreply.github.com>
2021-10-13 20:37:04 +0300

    Update Ex02.py

--- Work2/Ex02.py
+++ Work2/Ex02.py
@@ -1,23 +1,20 @@
+import math
+
 class Rational:
     def __init__(self, num=1, den=1):
-        if isinstance(num, int) and isinstance(den, int) and den != 0:
-            self.__numerator, self.__denominator = num, den
-            if self.__denominator < 0:
-                self.__numerator = -self.__numerator
-                self.__denominator = -self.__denominator
-
-            gcd = 1
-            for i in range(1, max(self.__numerator if self.__numerator > 0 else -self.__numerator, self.__denominator)):
-                if self.__numerator % i == 0 and self.__denominator % i == 0:
-                    gcd = i
-
-            self.__numerator /= gcd
-            self.__denominator /= gcd
-        else:
-            raise BaseException
+        if not isinstance(num, int) or not isinstance(den, int):
+            raise TypeError("Invalid type of data entered.")
+        if den == 0:
+            raise ValueError("Denominator cannot be zero.")
+        self.__numerator, self.__denominator = num, den
+        if self.__denominator < 0:
+            self.__numerator, self.__denominator = -self.__numerator, -self.__denominator
+        num = math.gcd(self.__numerator, self.__denominator)
+        self.__numerator //= num
+        self.__denominator //= num
 
     def print_form(self):
-        return str(int(self.__numerator)) + "/" + str(int(self.__denominator))
+        return f"{self.__numerator}/{self.__denominator}"
 
     def print_float(self):
         return str(self.__numerator / self.__denominator)
@@ -26,15 +23,15 @@ class Rational:
 def main():
     ration = Rational(2, 4)
 
-    print(ration.print_float() + "\n" + ration.print_form() + "\n")
+    print(f"{ration.print_float()}\n{ration.print_form()}\n")
     try:
         ration = Rational("5", "no")
-    except BaseException:
+    except TypeError as e:
         print("Invalid types of data entered in class instance\n")
     try:
         ration = Rational(5, 0)
-    except BaseException:
+    except ValueError as e:
         print("Division on zero.\n")
 
-
+        
 main()
