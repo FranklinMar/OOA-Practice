4369318df854fb60f5ee9d2167d78e2329e903a9
FranklinMar <49430756+FranklinMar@users.noreply.github.com>
2021-10-19 20:35:01 +0300

    Update Ex04.py

--- Work2/Part2/Ex04.py
+++ Work2/Part2/Ex04.py
@@ -1,48 +1,54 @@
 class BinaryTree:
+    """
+    Instance of Class contains codes of the products and their prices
+    in binary tree. Constructor creates a node with data.
+    Method 'insert' sorts input data by product codes and
+    inserts it into the tree in a form of a node.
+    Method 'get_price' returns price of a product,
+    which is searched by product code, or None
+    if a product with such a code doesn't exist.
+    """
     product_code = None
     price = 0
     left = right = None
 
     def __init__(self, product, prc):
-        if isinstance(product, int) and isinstance(prc, int):
-            self.product_code = product
-            self.price = prc
-        else:
+        if not isinstance(product, int) or not isinstance(prc, (int, float)):
             raise TypeError("Invalid type of data was entered.")
+        self.product_code = product
+        self.price = prc
 
     def insert(self, product, prc):
-        if isinstance(product, int) and isinstance(prc, int):
-            if self.product_code == product:
-                return
-            if self.product_code > product:
-                if self.left is None:
-                    self.left = BinaryTree(product, prc)
-                else:
-                    self.left.insert(product, prc)
-                return
-            if self.right is None:
-                self.right = BinaryTree(product, prc)
-            else:
-                self.right.insert(product, prc)
-        else:
+        if not isinstance(product, int) or not isinstance(prc, (int, float)):
             raise TypeError("Invalid type of data was entered.")
+        if self.product_code == product:
+            return
+        if self.product_code > product:
+            if self.left is None:
+                self.left = BinaryTree(product, prc)
+            else:
+                self.left.insert(product, prc)
+            return
+        if self.right is None:
+            self.right = BinaryTree(product, prc)
+        else:
+            self.right.insert(product, prc)
 
     def get_price(self, product):
-        if isinstance(product, int):
-            if self.product_code == product:
-                return self.price
-            elif self.product_code > product:
-                if self.left is None:
-                    return None
-                else:
-                    return self.left.get_price(product)
-            else:
-                if self.right is None:
-                    return None
-                else:
-                    return self.right.get_price(product)
-        else:
+        if not isinstance(product, int):
             raise TypeError("Invalid type of data was entered.")
+        if self.product_code == product:
+            return self.price
+        elif self.product_code > product:
+            if self.left is None:
+                return None
+            else:
+                return self.left.get_price(product)
+        else:
+            if self.right is None:
+                return None
+            else:
+                return self.right.get_price(product)
 
 
 def main():
@@ -50,31 +56,30 @@ def main():
     try:
         price_tree.get_price('2')
     except TypeError as e:
-        print("Yes")
+        print(e)
     price_tree.insert(2, 25)
     try:
         price_tree.insert('2', 5)
     except TypeError as e:
-        print("Yes")
+        print(e)
     try:
         price_tree.insert(2, '5')
     except TypeError as e:
-        print("Yes\n")
-    
+        print(e)
     while True:
         inp = input("Enter ProductCode: ")
         if inp.isdigit():
             if price_tree.get_price(int(inp)) is not None:
                 break
             else:
-                print("This product code does not exist.", end="")
-        print("Wrong input!\n")
+                print("This product code does not exist.\n")  #, end="")
+        else:
+            print("Wrong input!\n")
     while True:
         quantity = input("Enter Quantity: ")
-        if quantity.isdigit() and eval(quantity) >= 0:
+        if quantity.isdigit():
             break
         print("Wrong input!\n")
-
     print(f"Price of product: {price_tree.get_price(int(inp))}\n" +
           f"Cost of all products: {price_tree.get_price(int(inp)) * int(quantity)}")
 
